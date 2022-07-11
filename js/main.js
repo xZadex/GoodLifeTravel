20b470450a4de28d4d84cbb2694be267b9ff9895
Zade <codebyzade@gmail.com>
2022-07-11 00:45:19 -0400

    fixed mobile menu / some responsiveness fixes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 //mobile menu
 
-function show(){
-   let display = document.getElementsById("mobile-nav-list").style.display;
-   console.log(display);
-}
+function toggleMenu() {
+
+   let mobileMenu = document.getElementById('mobile-nav-list');
+   let displaySetting = mobileMenu.style.display;
+   let menuButton = document.getElementById('hamburger');
+
+   if (displaySetting == 'flex') {
+     mobileMenu.style.display = 'none';
+     menuButton.className = 'fa-solid fa-bars';
+   }
+   else {
+     mobileMenu.style.display = 'flex';
+     menuButton.className = 'fa-solid fa-xmark';
+   }
+ }
